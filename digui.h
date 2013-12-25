51f76b7cee3eba8ec7b9d5913c38ae2dd2248ff0
youzipi <blake_12@qq.com>
2013-12-25 21:09:52 +0800

    递归算法修改
    putchar（）替换printf（）
    判断条件由（x > 0）改为（x >= n）

--- digui.h
+++ digui.h
@@ -1,17 +1,24 @@
 #include<stdio.h>
 
 void M2X_dg(int x){
-	int t;
+	int r;
 	//while(x){
-	if(x){
-		//x = x / n;
-		M2X_dg(x / n);
-		t = x % n;
+	//if(x){
+	//	//x = x / n;
+	//	M2X_dg(x / n);
+	//	t = x % n;
 
-		if(t > 9)
-			printf("%c",t - 10 + 'A');
-		else
-			printf("%c",t + '0');
-		
-	}//while
+	//	if(t > 9)
+	//		printf("%c",t - 10 + 'A');
+	//	else
+	//		printf("%c",t + '0');
+	//	
+	//}//while
+	r = x % n;
+	if(x >= n)
+		M2X_dg(x / n);
+	if(r < 10)
+		putchar(r + '0');
+	else
+		putchar(r - 10 +'A');
 }
